00bf512d53987f823b575751aea8417cdce59d53
johnny <johnny@wantgoo.com>
2021-11-05 17:42:21 +0800

    create project and add sample code

---
+++ src/PlaywrightLab/PlaywrightLab/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.Playwright;
+
+namespace PlaywrightLab
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            using var playwright = await Playwright.CreateAsync();
+            //await using var browser = await playwright.Chromium.LaunchAsync(new BrowserTypeLaunchOptions
+            //{
+            //    ExecutablePath = @"C:\Program Files (x86)\Google\Chrome\Application\chrome.exe",
+            //    Channel = "chrome",
+            //    Headless = false
+            //});
+            await using var browser = await playwright.Chromium.LaunchPersistentContextAsync(
+                @"C:\Users\wantgoo\AppData\Local\Google\Chrome\User Data\Profile 2"
+                , new BrowserTypeLaunchPersistentContextOptions
+                {
+                    ExecutablePath = @"C:\Program Files (x86)\Google\Chrome\Application\chrome.exe",
+                    Channel = "chrome",
+                    Headless = false
+                });
+
+            var page = await browser.NewPageAsync();
+            await page.GotoAsync("https://playwright.dev/dotnet");
+            await page.WaitForTimeoutAsync(10000);
+            var a = await page.ScreenshotAsync(new PageScreenshotOptions { Path = "screenshot.png" });
+
+
+            await File.WriteAllBytesAsync(@"D:\test.png", a);
+
+            //Console.Read();
+        }
+    }
+}
